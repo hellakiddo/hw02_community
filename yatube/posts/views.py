16d4dafa5c8758e6446e9b5f3067890868e29067
A.M <113894011+Amir800S@users.noreply.github.com>
2022-12-02 12:59:08 +0300

    second commit

---
+++ yatube/posts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Post, Group
+
+# Create your views here.
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+        'title': 'Последние обновления на сайте',
+    }
+    return render(request, 'posts/index.html', context)
+
+
+# Create your views here.
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': 'Записи сообщества'
+
+    }
+    return render(request, 'posts/group_list.html', context)
